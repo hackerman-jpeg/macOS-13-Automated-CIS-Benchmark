a3b90c6ff96f6c17e36da8eb550dde183c553ee1
hackerman.jpeg <41294610+dimaswell@users.noreply.github.com>
2023-04-19 11:45:35 -0500

    added file

---
+++ helper_functions/main.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# macOS CIS Benchmarks Automation - Main Script
+# This script automates the CIS benchmarks checks for macOS 13 Ventura.
+# It sources the required helper functions and check scripts, then runs the checks.
+
+# Source the helper functions
+source "./helper_functions/helper_functions.sh"
+
+# Source the check scripts
+# Replace the paths with the correct ones if I change
+source "./checks/section_1/check_1_1.sh"
+source "./checks/section_1/check_1_2.sh"
+source "./checks/section_1/check_1_3.sh"
+source "./checks/section_1/check_1_4.sh"
+source "./checks/section_1/check_1_5.sh"
+source "./checks/section_1/check_1_6.sh"
+source "./checks/section_1/check_1_7.sh"
+
+# Add more source lines for additional check scripts as needed
+
+# Run the checks
+# I need to add a line for each check function that needs to be called
+check_1_1
+check_1_2
+check_1_3
+check_1_4
+check_1_5
+check_1_6
+check_1_7
+
+# Add more check function calls for additional checks as needed
+
+# Print the summary of the check results
+print_summary
